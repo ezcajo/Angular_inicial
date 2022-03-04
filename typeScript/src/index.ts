aee472f6503571025b08b857a4eb710f0c8dbd40
ezcajo <ezcajo41@yahoo.com.ar>
2022-03-04 18:33:20 -0300

    01-tipos basicos

---
+++ typeScript/src/index.ts
@@ -0,0 +1,10 @@
+/*
+  //  ===== Código de TypeScript =====
+*/
+
+let nombre: string = "Marcelo";
+let apellido: string | number;
+apellido = "Peralta";
+apellido = 33;
+
+console.log(`${nombre} ${apellido}`)
