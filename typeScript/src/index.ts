e69527c856dae7a1568dadc1534a03f2153a4cd0
ezcajo <ezcajo41@yahoo.com.ar>
2022-03-04 19:47:38 -0300

    02- Arreglos- Objetos - Interface

--- typeScript/src/index.ts
+++ typeScript/src/index.ts
@@ -1,10 +1,36 @@
-/*
-  //  ===== Código de TypeScript =====
-*/
+/**
+ * Esta variable se determina el tipo de dato que almacenara el arreglo, no recomendable
+ * se debe almacenar de un solo tipo
+ */
+let habilidades: (string| number)[] =["tecnica", "comprension","linguisticas"];
 
-let nombre: string = "Marcelo";
-let apellido: string | number;
-apellido = "Peralta";
-apellido = 33;
+habilidades.push(1);
 
-console.log(`${nombre} ${apellido}`)
+
+let temperaturas: number[] =[10,23,32,18];
+let sumaTotal : number = 0;
+
+for(let temperatura of temperaturas){
+  console.log(temperatura);
+  sumaTotal+=temperatura;
+}
+console.log(`temperatura total: ${sumaTotal}`);
+
+/**
+ * Interfaces y Objetos
+ */
+interface Persona{
+  nombre:string;
+  apellido: string;
+  temperatura: number[];
+  edad: number;
+  pueblonatal?: string;
+}
+const persona : Persona ={
+  nombre: "Marcelo",
+  apellido: "Peralta",
+  temperatura: temperaturas,
+  edad: 53
+}
+persona.pueblonatal ="San Martin"
+console.table(persona)
