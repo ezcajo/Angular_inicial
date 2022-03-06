cb965c556e10035513cc4a812a1b757d79b193d5
ezcajo <ezcajo41@yahoo.com.ar>
2022-03-06 13:54:23 -0300

    05- Desectruturacion de Objetos

--- typeScript/src/index.ts
+++ typeScript/src/index.ts
@@ -1,34 +1,42 @@
 /**
- * Objetos e Interfaces
  * 
+ * Destructuracion de Objetos
  */
- 
-//interface
-interface SuperHeroes{
-nombre:string;
-edad: number;
-direccion: Direccion; // interface de direccion
-mostrarDireccion: () => string;
-}
-interface Direccion{
-    calle: string;
-    pais: string;
-    ciudad: string;
+
+interface Reproductor{
+    volumen: number;
+    segundo : number;
+    cancion: string, 
+    detalles: Detalles;
 }
 
-//objeto superheroe
-const heroe01: SuperHeroes = {
-    nombre: 'Marcelo',
-    edad: 53,
-    direccion: {
-        calle: 'Francia',
-        pais: 'Argentina',
-        ciudad: 'San Martin',
-    },
-    mostrarDireccion: function (): string {
-     return (`${this.nombre} ${this.direccion.calle} ${this.direccion.ciudad}`);
+interface Detalles{
+    autor: string;
+    anio: number;
+}
+
+const reproductor: Reproductor={
+    volumen: 90,
+    segundo: 36,
+    cancion: "Angel",
+    detalles: {
+        autor: "Gary",
+        anio: 1991
     }
-
 }
-//impresion del metodo mostrar direccion objeto heroe01
-console.log(heroe01.mostrarDireccion());
+//destructuracion de objetos
+const{volumen, segundo, cancion, detalles} = reproductor;
+const{autor, anio} = detalles
+
+//uso de constantes desestructurada
+document.body.innerHTML=( `<div>
+                <hr>
+                volumen: ${volumen}<br>
+                segundo: ${segundo} <br>
+                cancion: ${cancion} <br>
+                autor: ${autor}<br>
+                año:  ${anio}<br>
+                </div><br>
+                <hr>
+                                    `)
+                                    
