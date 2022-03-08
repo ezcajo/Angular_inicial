0bf0b0a4d7b7b01d1e8029ccec9cc4f0ce8afde8
Jonathan Peralta <ezcajo41@yahoo.com.ar>
2022-03-08 00:50:43 -0300

    actualizacion

--- typeScript/src/index.ts
+++ typeScript/src/index.ts
@@ -6,7 +6,7 @@
 class Persona{
  
     /**
-     * El metodo constructor de la maner que fue declarado me permite
+     * El metodo constructor de la manera que fue declarado me permite
      * declarar dentro del mismo los atributos de la clase y poder tambien asignarlo
      * cuando se construya la instancia del objeto
      */
