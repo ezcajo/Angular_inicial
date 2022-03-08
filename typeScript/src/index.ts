0d6f50b2faa07650c6a22f9b3777895440ee635f
ezcajo <ezcajo41@yahoo.com.ar>
2022-03-08 02:32:19 -0300

    17- Encadenamiento Opcional

--- typeScript/src/index.ts
+++ typeScript/src/index.ts
@@ -1,28 +1,29 @@
 /**
- * 
- * @param constructor Decoradores
- * @returns 
+ * Encadenamiento opcional
  */
 
 
-function reportableClassDecorator<T extends { new (...args: any[]): {} }>(constructor: T) {
-    return class extends constructor {
-      reportingURL = "http://www...";
-    };
-  }
-   
-  @reportableClassDecorator
-  class BugReport {
-    type = "reporte";
-    title: string;
-      reportingURL: any;
-   
-    constructor(t: string) {
-      this.title = t;
-    }
-  }
-   
-  const bug = new BugReport("Marcelo Peralta");
-  console.log(bug.title); // Prints "Needs dark mode"
-  console.log(bug.type); // Prints "report"
-  bug.reportingURL;
+interface Pasajero{
+    nombre: string;
+    hijos?: string[];/**El simbolo ? determina en esta caso un atributo opcional
+    puede suceder que haya pasajeros sin hijos */
+}
+
+const pasajero1: Pasajero ={
+    nombre: "Marcelo"
+}
+
+const pasajero2: Pasajero ={
+    nombre: "Jonathan",
+    hijos: ['Luis', 'Camila']
+}
+
+function cantidadHijos(pasajero:Pasajero): void{
+    console.log(pasajero.hijos?.length || 0);/**
+    se coloco el simbolo? para que no de error cuando se consulte por el pasajero que no
+    tiene hijo y funcionaria como un tipo if para determinar si la respuesta
+    es undefined que nos muestr el cero por que es mas grande que undefined
+    */
+}
+
+console.log(cantidadHijos(pasajero1))
